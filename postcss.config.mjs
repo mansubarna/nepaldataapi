5bee8b3086957ecdfd1d64e80eb385e9b6e29685
Subarna Man Maharjan <man.subarna@gmail.com>
2025-04-11 21:21:30 +0545

    added landing page

---
+++ postcss.config.mjs
@@ -0,0 +1,12 @@
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+  extend: {
+    colors: {
+      nepaliRed: "#DC143C",
+      nepaliBlue: "#003893",
+    },
+  },
+};
+export default config;
